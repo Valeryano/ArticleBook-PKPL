0306a555ee009ed972b942e628ee8013a8c5dd54
Valeryano <106950515+Valeryano@users.noreply.github.com>
2024-12-06 20:14:05 +0700

    Delete app directory

--- app/Models/Book.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Model;
-
-class Book extends Model
-{
-    protected $fillable = ['Judul', 'Penulis', 'ISBN_ISSN', 'Penerbit', 'Jumlah_Halaman', 'DOI','URL'];
-}
